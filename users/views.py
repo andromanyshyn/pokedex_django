db3a591a87df809c51192e1b1d7476035818a615
AndrewRomanyshyn <morisszalivaet228@mail.ru>
2023-04-20 21:42:22 +0200

    create registration form and page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+from .forms import RegistrationForm
+from .models import User
 
-# Create your views here.
+
+class LoginView(TemplateView):
+    template_name = 'users/login.html'
+
+
+class RegistrationView(SuccessMessageMixin, CreateView):
+    template_name = 'users/registration.html'
+    model = User
+    form_class = RegistrationForm
+    success_url = reverse_lazy('login')
+    success_message = 'Registration successful'
+
